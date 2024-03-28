252658e51ad13db4b7048aa972ea30237ebb05b8
Griffin Riley <rileyg@oregonstate.edu>
2024-03-28 10:24:19 -0700

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # AdventOfCode2023-Day17
+Solution to day 17 of Advent of Code 2023, in Python.
---
+++ solution.py
@@ -0,0 +1,9 @@
+
+
+
+def fileRead(name):
+    data = []
+    f = open(name, "r")
+    for line in f:
+        data.append(line);
+    return data
